f5b1e930a07b4135458fcd7583d357a19026cace
Melany Segarra <melanyk.segarra@ucuenca.edu.ec>
2024-11-18 11:37:05 -0500

    carga de imágenes y creación de celdas

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# wave_funtion_collapse
+# Wave Funtion Collapse
+
+`const celdas = [];` 
+`const azulejos = []` Array para las imágenes
+`const reglas = [];` Conjunto de elementos que componen las condiciones de los lados de las imágenes. Relacionado con la retícula.
+
+La variable RETICULA y la variable NA (número de azulejos debe estar en mayúscula), son variables sencillas. Número estático, que no va a variar nunca.
--- sketch.js
+++ sketch.js
@@ -1,7 +1,154 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+const celdas = []; // 4x4
+const RETICULA = 4; // número de lados, puede variar
+
+const azulejos = [];
+const NA = 15; // número de azulejos
+
+const reglas = [
+  // reglas de los bordes de cada azulejo
+  {
+    //tile 0
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 0,
+  },
+
+  {
+    //tile 1
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 0,
+  },
+
+  {
+    //tile 2
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 1,
+  },
+
+  {
+    //tile 3
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 1,
+  },
+
+  {
+    //tile 4
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 1,
+  },
+
+  {
+    //tile 5
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 0,
+  },
+
+  {
+    //tile 6
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 1,
+  },
+
+  {
+    //tile 7
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 0,
+  },
+
+  {
+    //tile 8
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 1,
+  },
+
+  {
+    //tile 9
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 1,
+  },
+
+  {
+    //tile 10
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 0,
+  },
+
+  {
+    //tile 11
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 1,
+  },
+
+  {
+    //tile 12
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 0,
+  },
+
+  {
+    //tile 13
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 0,
+  },
+
+  {
+    //tile 14
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 1,
+  },
+];
+
+function preload() {
+  for (let i = 0; i < NA; i++) {
+    //azulejos[i] = loadImage(`azulejos/tile${i}.png`);
+    azulejos[i] = loadImage("azulejos/tile" + i + ".png");
+  }
 }
 
-function draw() {
-  background(220);
+function setup() {
+  createCanvas(1080, 1080);
+
+  let opcionesI = [];
+  for (let i = 0; i < azulejos.length; i++) {
+    opcionesI.push(i);
+  }
+
+  for (let i = 0; i < RETICULA * RETICULA; i++) {
+    celdas[i] = {
+      colapsada: false,
+      opciones: opcionesI,
+    };
+  }
+  print(celdas);
 }
+
+function draw() {}
